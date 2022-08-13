d3e2f68181a1dcc0d7b655a2948d23de3a587f02
xFantik <phantom88@inbox.ru>
2022-08-13 21:14:19 +0300

    Initial commit

---
+++ desktop/src/ru/pb/gblibgdx/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package ru.pb.gblibgdx;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import ru.pb.gblibgdx.GB_LibGDX;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("My-GDX-Game");
+		new Lwjgl3Application(new GB_LibGDX(), config);
+	}
+}
